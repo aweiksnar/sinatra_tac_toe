bc4998164b179566404abe5620b027a9cf85a32a
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-25 12:18:58 -0500

    test suite

--- game.rb
+++ game.rb
@@ -2,5 +2,5 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get "/" do
-  "Welcome to tic-tac-toe"
+  erb :board
 end
---
+++ spec/game_spec.rb
@@ -0,0 +1,21 @@
+ENV['RACK_ENV'] = 'test'
+
+require "./game.rb"
+require "rspec"
+require "rack/test"
+require "spec_helper"
+
+describe "The tic tac toe game" do
+
+include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "should have a board layout" do
+    get "/"
+    expect(last_response).to be_ok
+  end
+
+end
