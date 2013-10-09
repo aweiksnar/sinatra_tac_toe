9ea8d98d6cfda058a0f3911ff7bd4592f4159c31
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-09 16:40:05 -0500

    preliminary UI setup

--- game.rb
+++ game.rb
@@ -1,11 +1,18 @@
 require "sinatra"
+require "sinc"
 require "sinatra/reloader" if development?
 
+enable :sessions
+
 get "/" do
+  session.clear
   erb :board
 end
 
 post "/" do
-  puts params
+  (1..9).each do |num|
+    session["space#{num}"] = params["space#{num}"] unless session["space#{num}"]
+  end
   erb :board
 end
+
