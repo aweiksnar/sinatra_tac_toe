15414778894fcd755e1f9e1b1c38d6f3fd377440
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-10 11:56:30 -0500

    add smart computer first move

--- game.rb
+++ game.rb
@@ -3,8 +3,10 @@ require "sinc"
 require "sinatra/reloader" if development?
 
 enable :sessions
+set :number_of_moves, 0
 
 get "/" do
+  settings.number_of_moves = 0
   session.clear
   erb :board
 end
@@ -13,6 +15,34 @@ post "/" do
   (1..9).each do |num|
     session["space#{num}"] = params["space#{num}"] unless session["space#{num}"]
   end
+  settings.number_of_moves += 1
+
+  id = BestComputerMove.new(session, settings.number_of_moves).fetch_best_move
+  session["space#{id}"] = "comp"
+
   erb :board
 end
 
+class BestComputerMove
+  def initialize(board_hash, number_of_moves)
+    @board = board_hash
+    @move_num = number_of_moves
+  end
+
+  def fetch_best_move
+    if @move_num == 1
+      FirstTurn.new(@board).get_best_move
+    end
+  end
+end
+
+class FirstTurn
+
+  def initialize(board_hash)
+    @board = board_hash
+  end
+
+  def get_best_move
+    @board[:space1] ? 5 : 1
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,42 @@ describe "The tic tac toe game" do
     get "/"
     expect(last_response).to be_ok
   end
+
+  it "should accept posts to the board" do
+    post "/"
+    expect(last_response).to be_ok
+  end
+end
+
+describe "the computer player" do
+  let(:comp) {BestComputerMove.new({}, 1)}
+
+  it "should exist" do
+    expect(comp).not_to be_nil
+  end
+
+  describe "#fetch_best_move" do
+    it "should exist" do
+      expect(comp).to respond_to(:fetch_best_move)
+    end
+
+    it "should return a number" do
+      expect(comp.fetch_best_move.is_a? Fixnum).to eq(true)
+    end
+  end
+end
+
+describe "FirstTurn" do
+  let(:first_turn_middle) {FirstTurn.new({:space5 => "user"})}
+  let(:first_turn_top_left) {FirstTurn.new({:space1 => "user"})}
+
+  describe "#get_best_move" do
+    it "top left if its open" do
+      expect(first_turn_middle.get_best_move).to eq(1)
+    end
+
+    it "middle if the top_left is filled" do
+      expect(first_turn_top_left.get_best_move).to eq(5)
+    end
+  end
 end
