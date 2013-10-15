71a1896b86f68a000654fe6a60cd1b870b42b311
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-15 10:39:00 -0500

    add assignment private methods to game controller

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -2,8 +2,7 @@ module Sinatra
   module GameController
     def alternate_first_move
       if settings.game_num % 2 == 0
-        id = MoveRouter.new(session, settings.number_of_moves).fetch_best_move
-        session["space#{id}"] = "comp"
+        assign_computer_move_to_board
         settings.number_of_moves += 1
       end
     end
@@ -15,18 +14,27 @@ module Sinatra
     end
 
     def make_user_move(params)
-      (1..9).each do |id|
-        session["space#{id}"] = params["space#{id}"] unless session["space#{id}"]
-      end
+      assign_user_move_to_board
       settings.number_of_moves += 1
     end
 
     def get_computer_move
-      id = MoveRouter.new(session, settings.number_of_moves).fetch_best_move
-      session["space#{id}"] = "comp"
+      assign_computer_move_to_board
       settings.number_of_moves += 1
       @game_over = MoveRouter.new(session, settings.number_of_moves).game_over?
     end
+
+    private
+
+    def assign_computer_move_to_board
+      id = MoveRouter.new(session, settings.number_of_moves).fetch_best_move
+      session["space#{id}"] = "comp"
+    end
+
+    def assign_user_move_to_board
+      (1..9).each {|id| session["space#{id}"] = params["space#{id}"] unless session["space#{id}"]}
+    end
+
   end
   helpers GameController
 end
