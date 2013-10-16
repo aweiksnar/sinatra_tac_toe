b4b1820db70a029b0b11848de250707af675b9bb
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-16 12:46:28 -0500

    change #reset_game to only clear board session

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -3,12 +3,12 @@ module Sinatra
     def alternate_first_move
       if settings.game_num % 2 == 0
         assign_computer_move_to_board
-        session[:number_of_moves] += 1
+        session[:number_of_moves] += 1             #  session[:game_num] = 0 unless session[:game_num]
       end
     end
 
     def reset_game
-      session.clear
+      clear_board                    #change to #clear_board which loops 1..9 and sets to nil so gamenum can increment
       session[:number_of_moves] = 0
       settings.game_num += 1
     end
@@ -25,6 +25,10 @@ module Sinatra
     end
 
     private
+
+    def clear_board
+      (1..9).each {|id| session["space#{id}"] = nil}
+    end
 
     def assign_computer_move_to_board
       id = MoveRouter.new(session, session[:number_of_moves]).fetch_best_move
